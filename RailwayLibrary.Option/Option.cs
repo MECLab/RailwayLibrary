f561f7c9ea1c426fa3d0982969e7ef98264493aa
Keagan Soakell-Ho <keagan.soakell-ho@clinician.io>
2017-08-26 14:12:17 +1200

    update build version prefix

--- RailwayLibrary.Option/Option.cs
+++ RailwayLibrary.Option/Option.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace RailwayLibrary.Option
+﻿namespace RailwayLibrary.Option
 {
     public abstract class Option<TContent>
     {
@@ -20,41 +18,4 @@ namespace RailwayLibrary.Option
     }
 
     public class None<TContent> : Option<TContent> { }
-
-    public static class OptionExtensions
-    {
-        public static Option<TContentOut> Map<TContentIn, TContentOut>(this Option<TContentIn> option, Func<TContentIn, TContentOut> map)
-        {
-            switch (option)
-            {
-                case Some<TContentIn> some:
-                    return new Some<TContentOut>(map(some.Content));
-                default:
-                    return new None<TContentOut>();
-            }
-        }
-
-        public static Option<TContentOut> FlatMap<TContentIn, TContentOut>(this Option<TContentIn> opt,
-            Func<TContentIn, Option<TContentOut>> flatMap)
-        {
-            switch (opt)
-            {
-                case Some<TContentIn> some:
-                    return flatMap(some.Content);
-                default:
-                    return new None<TContentOut>();
-            }
-        }
-
-        public static TContent GetOrDefault<TContent>(this Option<TContent> option, Func<TContent> defaultFunc)
-        {
-            switch (option)
-            {
-                case Some<TContent> some:
-                    return some.Content;
-                default:
-                    return defaultFunc();
-            }
-        } 
-    }
 }
