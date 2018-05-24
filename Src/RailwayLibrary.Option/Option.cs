378743ea1edc6ab96323aa426b7e5d8488f0a78b
Keagan Soakell-Ho <ksoakellho@clinictocloud.com.au>
2018-05-24 22:46:27 +1000

    Add extension helpers to Option type

--- Src/RailwayLibrary.Option/Option.cs
+++ Src/RailwayLibrary.Option/Option.cs
@@ -18,4 +18,23 @@
     }
 
     public class None<TContent> : Option<TContent> { }
+
+    public static class Option
+    {
+        public static Option<TContent> Some<TContent>(TContent content) => new Some<TContent>(content);
+
+        public static Option<TContent> None<TContent>() => new None<TContent>();
+
+        public static bool TryRetrieve<TContent>(this Option<TContent> option, out TContent content)
+        {
+            if (option is Some<TContent> some)
+            {
+                content = some.Content;
+                return true;
+            }
+
+            content = default(TContent);
+            return false;
+        }
+    }
 }
