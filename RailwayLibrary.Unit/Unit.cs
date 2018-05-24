e76df58041baf778927e200166f08026a9debed9
Keagan Soakell-Ho <ksoakellho@clinictocloud.com.au>
2018-05-24 10:00:56 +1000

    override ToString()

--- RailwayLibrary.Unit/Unit.cs
+++ RailwayLibrary.Unit/Unit.cs
@@ -1,8 +1,18 @@
-﻿using System;
-
-namespace RailwayLibrary.Unit
+﻿namespace RailwayLibrary.Unit
 {
-    public class Unit
+    /// <summary>
+    /// Unit type for use when returning nothing or void
+    /// </summary>
+    public sealed class Unit
     {
+        /// <summary>
+        /// The default <see cref="Unit"/> instance.
+        /// </summary>
+        public static readonly Unit Instance = new Unit();
+
+        /// <summary>
+        /// Gets "Unit" as a string value.
+        /// </summary>
+        public override string ToString() => nameof(Unit);
     }
 }
